ebe1163b5081dbfb15688eafc957203092d59335
quinnimgrund <quinnimgrund@gmail.com>
2023-11-14 20:13:31 -0800

    first commit, 2015 problem 1

--- 2015/1.c
+++ 2015/1.c
@@ -1,6 +1,83 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+char *read_file(char *filename);
+
+int floor_finder(char *file_contents);
 
 int main(void)
 {
-    printf("hello, world\n");
+    char *file_contents = read_file("1_puzzle_input.txt");
+    if (file_contents == NULL)
+    {
+        printf("Error reading file.\n");
+        return 1;
+    }
+
+    printf("%i\n", floor_finder(file_contents));
+
+    free(file_contents);
+
+    return 0;    
+}
+
+char *read_file(char *filename)
+{
+    FILE *file;
+
+    file = fopen(filename, "r");
+
+    if(!file) 
+    {
+        return NULL;
+    }
+
+    fseek(file, 0, SEEK_END);
+    int length = ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    char *string = malloc(sizeof(char) * (length+1));
+
+    char c;
+    int i = 0;
+    while ( (c = fgetc(file)) != EOF)
+    {
+        string[i] = c;
+        i++;
+    }
+
+    string[i] = '\0';
+
+    fclose(file);
+
+    return string;
+}
+
+int floor_finder(char *file_contents)
+{
+    int length = strlen(file_contents);
+    int answer = 0;
+    char up[] = "(";
+    char down[] = ")";
+
+    for (int i = 0; i < length; i++)
+    {
+        if (file_contents[i] == up[0])
+        {
+            answer += 1;
+            printf("add 1\n");
+        }
+        else if (file_contents[i] == down[0])
+        {
+            answer -= 1;
+            printf("subtract 1\n");
+        }
+        else
+        {
+            ;
+        }
+    }
+    return answer;
 }
